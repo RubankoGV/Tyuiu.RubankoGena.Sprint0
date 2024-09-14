a83c966fb897655e12fb394e25f5e5171724c239
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-09-15 00:23:53 +0300

    Task5

--- Tiuiu.RubankoGena.Sprint0.Task5.V0.Test/DataServiceTest.cs
+++ Tiuiu.RubankoGena.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -4,24 +4,23 @@ namespace Tiuiu.RubankoGena.Sprint0.Task5.V0.Test
 {
     public class DataServiceTest
     {
-        [SetUp]
         public void CheckedAdditionValid()
-        {
-            Assert.AreEqual(10, DataService.Addition(5, 5));
-        }
+            {
+                Assert.AreEqual(10, DataService.Addition(5, 5));
+            }
 
-        public void CheckedSubtractionValid()
-        {
-            Assert.AreEqual(5, DataService.Subtraction(10, 5));
-        }
+            public void CheckedSubtractionValid()
+            {
+                Assert.AreEqual(5, DataService.Subtraction(10, 5));
+            }
 
-        public void CheckedMultiplicationValid()
-        {
-            Assert.AreEqual(50, DataService.Multiplication(10, 5));
-        }
-        public void CheckedDivisionValid()
-        {
-            Assert.AreEqual(3, DataService.Division(9, 3));
+            public void CheckedMultiplicationValid()
+            {
+                Assert.AreEqual(50, DataService.Multiplication(10, 5));
+            }
+            public void CheckedDivisionValid()
+            {
+                Assert.AreEqual(3, DataService.Division(9, 3));
+            }
         }
     }
-}
