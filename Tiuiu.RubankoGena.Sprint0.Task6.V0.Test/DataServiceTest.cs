640976f4bc51f1d5679d4d28718f1f99bfac2ca3
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-09-16 20:26:56 +0300

    Task6

---
+++ Tiuiu.RubankoGena.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+using Tiuiu.RubankoGena.Sprint0.Task6.V0.Lib;
+namespace Tiuiu.RubankoGena.Sprint0.Task6.V0.Test
+{
+    public class Tests
+    {
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+
+        public void CheckSubtractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+
+        public void CheckMultArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
